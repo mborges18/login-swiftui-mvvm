7f9f830ee959dfc78651e77bbe60df7f34170bc5
mborges <desevolvedormbs@gmail.com>
2023-07-27 17:35:31 -0300

    Init project

---
+++ loginswiftui/SignUpView.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpView.swift
+//  loginswift
+//
+//  Created by Márcio Borges on 27/07/23.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text("SIGNUP")
+    }
+}
+
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
